79bd9f2db28d0056fbc43b58bd91c47b930114da
devnewton <devnewton@bci.im>
2019-10-30 15:53:34 +0100

    initial import

---
+++ NewtonAdventure.AppDir/newton_adventure.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -jar newton-adventure.jar
---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/sh
+echo "Download bundable opendjk"
+wget -c https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk11u-2019-10-16-10-43/OpenJDK11U-jdk_x64_linux_hotspot_2019-10-16-10-43.tar.gz
+tar xf OpenJDK11U-jdk_x64_linux_hotspot_2019-10-16-10-43.tar.gz 
+
+echo "Download and build Newton Adventure"
+git clone https://github.com/devnewton/newton_adventure.git newton_adventure
+(cd newton_adventure; mvn package)
+cp newton_adventure/game/lwjgl/target/newton-adventure.jar NewtonAdventure.AppDir/
+
+echo "Build custom jdk"
+NEWTON_ADVENTURE_DEPS=`./jdk-11.0.5+9/bin/jdeps --print-module-deps newton_adventure/game/lwjgl/target/newton-adventure.jar`
+./jdk-11.0.5+9/bin/jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules $NEWTON_ADVENTURE_DEPS --output NewtonAdventure.AppDir/usr
+
+wget -c https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage
+chmod +x appimagetool-x86_64.AppImage 
+./appimagetool-x86_64.AppImage NewtonAdventure.AppDir/
