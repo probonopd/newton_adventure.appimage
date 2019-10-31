79a52d656dd8625aed549381a4fb36cf549191b0
devnewton <devnewton@bci.im>
2019-10-31 15:51:07 +0100

    update clean

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm Newton_Adventure-x86_64.AppImage
-rm OpenJDK11U-jdk_x64_linux_hotspot_2019-10-16-10-43.tar.gz
+rm OpenJDK11U-jdk_x64_linux_hotspot_2019-10-31-00-22.tar.gz
 rm appimagetool-x86_64.AppImage
 rm -Rf jdk-11.0.5+9/
 rm -Rf NewtonAdventure.AppDir/usr
