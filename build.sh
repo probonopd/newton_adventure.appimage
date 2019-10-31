6e7e28fd81f8493d9a2e99e2d80980f3e1cab602
devnewton <devnewton@bci.im>
2019-10-31 16:02:24 +0100

    build folder

--- build.sh
+++ build.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
+echo "Create build directory"
+mkdir -p build
+cp -R Newton_Adventure.AppDir build/Newton_Adventure.AppDir
+cd build
+
 echo "Download bundable opendjk"
 wget -c https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk11u-2019-10-31-00-22/OpenJDK11U-jdk_x64_linux_hotspot_2019-10-31-00-22.tar.gz
 tar xf OpenJDK11U-jdk_x64_linux_hotspot_2019-10-31-00-22.tar.gz
-mv jdk-11.0.5+9 jdk
 
 echo "Download and build Newton Adventure"
 git clone https://github.com/devnewton/jnuit.git jnuit
 (cd jnuit; mvn install)
 git clone https://github.com/devnewton/newton_adventure.git newton_adventure
 (cd newton_adventure; mvn package)
-cp newton_adventure/game/lwjgl/target/newton-adventure-lwjgl-1.17-SNAPSHOT.jar NewtonAdventure.AppDir/newton-adventure.jar
+cp newton_adventure/game/lwjgl/target/newton-adventure-lwjgl-1.17-SNAPSHOT.jar Newton_Adventure.AppDir/newton-adventure.jar
 
 echo "Build custom jdk"
-NEWTON_ADVENTURE_DEPS=`./jdk-11.0.5+9/bin/jdeps --print-module-deps NewtonAdventure.AppDir/newton-adventure.jar`
-./jdk-11.0.5+9/bin/jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules $NEWTON_ADVENTURE_DEPS --output NewtonAdventure.AppDir/usr
+NEWTON_ADVENTURE_DEPS=`./jdk-11.0.5+9/bin/jdeps --print-module-deps Newton_Adventure.AppDir/newton-adventure.jar`
+./jdk-11.0.5+9/bin/jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules $NEWTON_ADVENTURE_DEPS --output Newton_Adventure.AppDir/usr
 
 wget -c https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage
 chmod +x appimagetool-x86_64.AppImage
-ARCH=x86_64 ./appimagetool-x86_64.AppImage NewtonAdventure.AppDir/
+ARCH=x86_64 ./appimagetool-x86_64.AppImage Newton_Adventure.AppDir/ 
--- clean.sh
+++ clean.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-rm Newton_Adventure-x86_64.AppImage
-rm OpenJDK11U-jdk_x64_linux_hotspot_2019-10-31-00-22.tar.gz
-rm appimagetool-x86_64.AppImage
-rm -Rf jdk-11.0.5+9/
-rm -Rf NewtonAdventure.AppDir/usr
-rm -Rf newton_adventure/
+rm -Rf build
