cc75ba0923fb265d81e2b99efecf787ae1db920f
devnewton <devnewton@bci.im>
2019-10-31 15:49:12 +0100

    fix build

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo "Download bundable opendjk"
-wget -c https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk11u-2019-10-16-10-43/OpenJDK11U-jdk_x64_linux_hotspot_2019-10-16-10-43.tar.gz
-tar xf OpenJDK11U-jdk_x64_linux_hotspot_2019-10-16-10-43.tar.gz 
+wget -c https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk11u-2019-10-31-00-22/OpenJDK11U-jdk_x64_linux_hotspot_2019-10-31-00-22.tar.gz
+tar xf OpenJDK11U-jdk_x64_linux_hotspot_2019-10-31-00-22.tar.gz
+mv jdk-11.0.5+9 jdk
 
 echo "Download and build Newton Adventure"
 git clone https://github.com/devnewton/jnuit.git jnuit
@@ -11,11 +12,9 @@ git clone https://github.com/devnewton/newton_adventure.git newton_adventure
 cp newton_adventure/game/lwjgl/target/newton-adventure-lwjgl-1.17-SNAPSHOT.jar NewtonAdventure.AppDir/newton-adventure.jar
 
 echo "Build custom jdk"
-mkdir NewtonAdventure.AppDir/usr
 NEWTON_ADVENTURE_DEPS=`./jdk-11.0.5+9/bin/jdeps --print-module-deps NewtonAdventure.AppDir/newton-adventure.jar`
 ./jdk-11.0.5+9/bin/jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules $NEWTON_ADVENTURE_DEPS --output NewtonAdventure.AppDir/usr
 
-cp NewtonAdventure.AppDir/newton_adventure.sh NewtonAdventure.AppDir/usr/bin/
 wget -c https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage
-chmod +x appimagetool-x86_64.AppImage 
+chmod +x appimagetool-x86_64.AppImage
 ARCH=x86_64 ./appimagetool-x86_64.AppImage NewtonAdventure.AppDir/
